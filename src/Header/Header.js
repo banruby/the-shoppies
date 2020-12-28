d1360f976056a4488098509bf138bd0f7b6a9741
Ruby Bantock <banruby@gmail.com>
2020-12-28 12:54:47 -0500

    initial file structure

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+
+    return (
+        <header>
+            <div className="wrapper">
+                <p>The Shoppies!</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
